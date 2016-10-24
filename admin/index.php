8374e57eb43508427aee5d2d90a901a9c0220003
Luis Navarro <svsmart@users.noreply.github.com>
2016-10-23 22:23:40 -0600

    Tienda

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,4 @@
+<h1>Panel Administrativo</h1>
 <a href="pedidos.php" title="Gestionar Pedidos">Gestionar Pedidos</a>
 <a href="clientes.php" title="Gestionar Clientes">Gestionar Clientes</a>
 <a href="productos.php" title="Gestionar Productos">Gestionar Productos</a>
