b7b71f41d7ed9144eef62596617e12ea268e9e39
Luis Navarro <svsmart@users.noreply.github.com>
2016-10-28 20:07:59 -0600

    Tienda ElectroDigital

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,16 @@
-<h1>Panel Administrativo</h1>
-<a href="pedidos.php" title="Gestionar Pedidos">Gestionar Pedidos</a>
-<a href="clientes.php" title="Gestionar Clientes">Gestionar Clientes</a>
-<a href="productos.php" title="Gestionar Productos">Gestionar Productos</a>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Panel Administrativo</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<h1>Panel Administrativo</h1>
+  <a href="pedidos.php" title="Gestionar Pedidos">Gestionar Pedidos</a>
+  <a href="clientes.php" title="Gestionar Clientes">Gestionar Clientes</a>
+  <a href="productos.php" title="Gestionar Productos">Gestionar Productos</a>
+</body>
+</html>
+
